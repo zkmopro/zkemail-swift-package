db941a7965d5d6121794449b443a89134715f517
moven0831 <moven0831@gmail.com>
2025-04-15 12:26:59 +0800

    chore: update Package.swift and ci

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "MoproFFI",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v15),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MoproFFI",
             targets: ["MoproFFI"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MoproFFI",
             dependencies: [
@@ -22,14 +23,15 @@ let package = Package(
             path: "Sources/"),
         .binaryTarget(
             name: "mopro",
-            path: "Sources/MoproiOSBindings/MoproBindings.xcframework"
+            path: "Sources/MoproiOSBindings/MoproBindings.xcframework.zip"
         ),
         .testTarget(
             name: "MoproFFITests",
             dependencies: ["MoproFFI"],
             path: "Tests/",
             resources: [
-                .process("MoproAssets/multiplier2_final.zkey"),
+                .process("MoproAssets/zkemail_input.json"),
+                .process("MoproAssets/srs.local")
             ]
         )
     ]
